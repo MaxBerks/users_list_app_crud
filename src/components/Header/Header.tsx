acdf3fc11c6c97ee6a120e7f6e0ce2fe7418f862
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-11 12:39:55 +0100

    feat: implement Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.scss'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className="header__title">Users list</div>
+    </div>
+  )
+}

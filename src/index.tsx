e44de164a90dfe24f5ca476c3867939bea2972fe
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-11 12:39:05 +0100

    fix: add nullstyle.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
+import './nullstyle.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(

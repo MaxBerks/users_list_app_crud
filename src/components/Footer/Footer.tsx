4faf5bfd85c97081564930e74fc3012e4e0ecc51
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-15 19:27:09 +0100

    fix: rename classNames to BEM technology

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ import { PiMouseSimpleLight } from "react-icons/pi";
 export default function Footer() {
   return (
     <footer className='footer'>
-      <ul className="footer_dots">
-        <li className="dots_dot"></li>
-        <li className="dots_dot dots_dot__active"></li>
-        <li className="dots_dot"></li>
+      <ul className="footer__dots">
+        <li className="dots__dot"></li>
+        <li className="dots__dot dots__dot-active"></li>
+        <li className="dots__dot"></li>
       </ul>
-      <PiMouseSimpleLight className='footer_mouse'/>
+      <PiMouseSimpleLight className='footer__mouse'/>
     </footer>
   )
 }

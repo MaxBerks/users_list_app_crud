e31485ae0362ac9b649dbf51b4a6ac6033f63aae
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-15 19:13:35 +0100

    feat: implement Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+// import Main from './components/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='content'>
+        <div></div>
+        <Footer />
+      </div>
     </div>
   );
 }

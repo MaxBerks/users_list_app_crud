40c3b78318e5deab39684db15bf609e34afb8e65
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-15 19:18:25 +0100

    feat: add Main

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-// import Main from './components/Main/Main'
+import Main from './components/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className='content'>
-        <div></div>
+        <Main />
         <Footer />
       </div>
     </div>

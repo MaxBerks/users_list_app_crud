eb456d9fef69d497586826a8443de9c840b8148d
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-15 19:31:11 +0100

    feat: add left and right buttons

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./Main.scss"
+import { IoIosArrowForward, IoIosArrowBack  } from "react-icons/io";
+
 
 export default function Main() {
   return (
-    <main className='main'></main>
+    <main className='main'>
+      <IoIosArrowBack className='main__btn main__btn-left'/>
+      <div></div>
+      <IoIosArrowForward className='main__btn main__btn-right'/>
+    </main>
   )
 }

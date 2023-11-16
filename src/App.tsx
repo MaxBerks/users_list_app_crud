49ba7d33b66de8b55f758c78cef93fd3cb5293c3
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-11-16 12:39:49 +0100

    feat: add fetching users data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 import Header from './components/Header/Header'
-import Main from './components/Main/Main'
+import Main from './components/Main/Main' 
+
+type userType = {
+  id: number,
+  email: string,
+  first_name: string,
+  last_name: string,
+  avatar: string
+}
+
+type apiGetUsersResponseType = {
+  page: number,
+	per_page: number,
+	total: number,
+	total_pages: number,
+	data: userType[]
+}
+
+async function getUsers(apiUrl: string) : Promise<string | userType[]> {
+  try {
+    const { data } = await axios.get<apiGetUsersResponseType>(
+      apiUrl,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    // console.log(JSON.stringify(data.data, null, 4));
+    // console.log(JSON.stringify(data.data));
+    return data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
 
 function App() {
+  const apiUrl = "https://reqres.in/api/users?page=1"; 
+  const [userList, setUserList] = useState<userType[]>([]);
+
+    useEffect(() => {
+      getUsers(apiUrl).then((res: string | userType[])=>{
+        if(typeof res === 'object') {
+          setUserList(res);
+          // console.log(res);
+        }
+        else console.log("Api response error!");
+      });
+    }, []);
+
   return (
     <div className="App">
       <Header />
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Main.scss"
 import { IoIosArrowForward, IoIosArrowBack  } from "react-icons/io";
 import CardSlider from '../CardSlider/CardSlider'
 import Footer from '../Footer/Footer'
 
+
+
+const nextSlide = () => {
+
+}
+
+const prevSlide = () => {
+
+}
+
 export default function Main() {
+  const [slideIndex, setSlideIndex] = useState(2);
+
   return (
     <main className='main'>
       <div className="main__content">
-        <IoIosArrowBack className='main__btn main__btn-left'/>
+        <IoIosArrowBack className='main__btn main__btn-left' onClick={prevSlide}/>
         <CardSlider />
-        <IoIosArrowForward className='main__btn main__btn-right'/>
+        <IoIosArrowForward className='main__btn main__btn-right' onClick={nextSlide}/>
       </div>
       <Footer />
     </main>
